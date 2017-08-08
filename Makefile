30b97a29f0a5d23d128c1d5a17a6621bf8053474
Aleksandr Leonov <leonov.aleksandr.1987@gmail.com>
2017-08-08 19:50:06 +0300

    First version of golosdata package.

---
+++ Makefile
@@ -0,0 +1,16 @@
+clean: clean-build clean-pyc
+
+clean-build:
+	rm -fr build/ dist/ *.egg-info .eggs/ .tox/ __pycache__/ .cache/ .coverage htmlcov src
+
+clean-pyc:
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+
+install: clean
+	pip install -e .
+
+pypi:
+	python setup.py bdist_wheel --universal
+	python setup.py sdist bdist_wheel upload
---
+++ setup.py
@@ -0,0 +1,43 @@
+import sys
+from codecs import open
+from os import path
+
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+assert sys.version_info[0] == 3, "golosdata requires Python > 3"
+
+VERSION = '0.0.1'
+
+setup(
+    name='golosdata',
+    version=VERSION,
+    description='Python Utilities for parsing GOLOS blockchain',
+    long_description=open('README.md').read(),
+    url='https://github.com/pmartynov/golosdata',
+    author='@steepshot',
+    author_email='steepshot.org@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='golos golosio',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=[
+        # 'steem',  # steem lib for golos
+        'pymongo',
+        'requests',
+        'funcy',
+        'werkzeug',
+    ],
+
+    extras_require={
+        'dev': [''],
+        'test': [''],
+    },
+)
